091385d0d9922a90366b4b7f73176711705c97fa
prince0290 <73387901+prince0290@users.noreply.github.com>
2020-11-22 16:02:39 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,22 @@
+class Box{
+    constructor(x,y,width,height){
+        var options = {restitution : 1};
+        this.body=Bodies.rectangle(x,y,width,height);
+        World.add(world,this.body);
+        this.width=width;
+        this.height=height;
+    }
+display(){
+    var pos  = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x,pos.y);
+    angleMode(RADIANS);
+    rotate (angle);
+    rectMode(CENTER);
+    fill ("yellow");
+    rect (0,0,this.width,this.height)
+    pop();
+}
+
+}
